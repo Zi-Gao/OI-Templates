4f74b4faf1014076a24529a61c7c602eb5095fa6
Zi-Gao <gyc.zachary@gmail.com>
2025-06-05 20:39:09 +0800

    index from 0

--- Templates/Data structures/UF.cpp
+++ Templates/Data structures/UF.cpp
@@ -1,7 +1,7 @@
 const int UFDS_SIZE=100010;
 struct UF{
 	int par[UFDS_SIZE];
-	void build(int n){for(register int i=1;i<=n;++i)par[i]=i;}
+	void build(int n){for(int i=0;i<=n;++i)par[i]=i;}
     template<int dep> inline __attribute((always_inline)) constexpr int fold(int u){
         if constexpr (dep==0) return par[u];
         else return par[u]=fold<dep-1>(par[u]);
